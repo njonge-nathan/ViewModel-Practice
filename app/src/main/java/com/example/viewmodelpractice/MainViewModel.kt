c8a7d996b694f77713e2ec2f2a5901f0affa9809
NATE <nathanwaithaka24@gmail.com>
2024-09-28 11:53:04 +0300

    Added comments on the ViewModel Class

--- app/src/main/java/com/example/viewmodelpractice/MainViewModel.kt
+++ app/src/main/java/com/example/viewmodelpractice/MainViewModel.kt
@@ -6,5 +6,6 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+    // this handles the state for the textfield input
     var text: String by mutableStateOf("")
 }
